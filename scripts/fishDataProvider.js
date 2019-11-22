07db302f10fde3075f57ef162733470cdcac8db8
AdiGarmendia <dejima@gmail.com>
2019-11-22 10:53:18 -0600

    added function array

---
+++ scripts/fishDataProvider.js
@@ -0,0 +1,66 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const fishCollection = [
+  {
+    name: "Sam",
+    food: ["Royals With Cheese", "Gourmet Coffee"],
+    species: "Not a Sea McNugget",
+    location: "USA",
+    length: "6'2",
+    imgage: "sam.jpg"
+  },
+  {
+    name: "Glass",
+    food: ["Smoothies", "Gourmet Coffee"],
+    species: "Not a Sea McNugget",
+    location: "USA",
+    length: "6'2",
+    image: "glass.jpg"
+  },
+  {
+    name: "Laz",
+    food: ["Metamucil", "Gourmet Coffee"],
+    species: "Not a Sea McNugget",
+    location: "USA",
+    length: "6'2",
+    image: "laz.jpg"
+
+},
+  {
+    name: "Major",
+    food: ["Bounties", "Gourmet Coffee"],
+    species: "Not a Sea McNugget",
+    location: "USA",
+    length: "6'2",
+    image: "major.jpg"
+
+},
+  {
+    name: "Shaft",
+    food: ["Suckas", "Gourmet Coffee"],
+    species: "Not a Sea McNugget",
+    location: "USA",
+    length: "6'2",
+    image: "shaft.jpg"
+
+
+},
+  {
+    name: "Fury",
+    food: ["Hydra", "Gourmet Coffee"],
+    species: "Not a Sea McNugget",
+    location: "USA",
+    length: "6'2",
+    image: "fury.jpg"
+
+}
+]
+
+// This is new code. Add this.
+export const useFish = () => {
+  return fishCollection
+}
--- scripts/main_index.js
+++ scripts/main_index.js
@@ -5,8 +5,17 @@
 import initializeDetailButtonEvents from './dialog.js'
 import addMouseOverEventListeners from './highlight.js'
 import addMouseOverEventListenersTips from './highTips.js'
+import { useFish } from './fishDataProvider.js'
 
 
 initializeDetailButtonEvents()
 addMouseOverEventListeners()
 addMouseOverEventListenersTips()
+
+const allOfTheFish = useFish()
+
+console.log("all of the fish:", allOfTheFish)
+
+for (const fish of allOfTheFish) {
+  console.log("individual fish object:", fish)
+}
